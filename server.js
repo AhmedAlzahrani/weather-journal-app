a6eedef6c2b4211ec7d21e945c5b99f4a04bcfae
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-07-02 14:36:03 +0300

    setting up node & express. established a successful connection with the api

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+
+
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +17,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+//PORT number
+const PORT = 1200;
+
+//Listening to PORT
+const listening = app.listen(PORT , (req , res) => {
+    console.log("Server Running");
+    console.log("Running on http://localhost:1200");
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,28 @@
 /* Global Variables */
 
+// API key
+const APIKey = '9dc214b2626c9ab862c5cee625198586';
+const url = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const weather = async (url='' , key='') => {
+    console.log(url+key)
+    const result = await fetch(url+key);
+
+    try{
+        JSON.stringify(result);
+        console.log(result)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// Adding event listener to the button with the id generate
+
+document.getElementById('generate').addEventListener('click' , () => {
+    const zip = document.getElementById('zip').value;
+    weather(url + zip + '&appid=',APIKey);
+})
